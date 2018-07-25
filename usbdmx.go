d70d641a0b35193fb52edac7d3fb5c9f29dd26f7
Oli Read <oli.read55@gmail.com>
2018-07-25 12:08:44 +0100

    Updates usbdmx/Controller interface to match the new spec introduced by the Enttec controller

--- usbdmx.go
+++ usbdmx.go
@@ -13,8 +13,8 @@ type Controller interface {
 	Connect() (err error)
 	GetSerial() (info string, err error)
 	GetProduct() (info string, err error)
-	SetChannel(channel int16, value byte) error
-	GetChannel(channel int16) (byte, error)
+	SetChannel(universe int16, channel int16, value byte) error
+	GetChannel(universe int16, channel int16) (byte, error)
 	Render() error
 }
 
